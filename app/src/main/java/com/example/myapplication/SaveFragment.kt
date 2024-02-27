44de1e8f3383692357bfb11a57e45f46c8088f57
Maria Fernanda Morales <marifer160204@gmail.com>
2024-02-26 19:41:32 -0600

    Layout landscapes and life cycle finished

--- app/src/main/java/com/example/myapplication/SaveFragment.kt
+++ app/src/main/java/com/example/myapplication/SaveFragment.kt
@@ -1,17 +1,45 @@
 package com.example.myapplication
 
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 
 class SaveFragment : Fragment() {
+    val TAG = "HomeFragment"
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
+        Log.d(TAG,"onCreate: ")
         return inflater.inflate(R.layout.fragment_save, container, false)
+    }
+    override fun onStart() {
+        super.onStart()
+        Log.d(TAG,"onStart: ")
+    }
+
+    override fun onResume(){
+        super.onResume()
+        Log.d(TAG,"onResume: ")
+    }
+
+    override fun onPause() {
+        super.onPause()
+        Log.d(TAG,"onPause: ")
+    }
+
+    override fun onStop() {
+        super.onStop()
+        Log.d(TAG,"onStop: ")
+    }
+
+
+    override fun onDestroy() {
+        super.onDestroy()
+        Log.d(TAG,"onDestroy :")
     }
 }
