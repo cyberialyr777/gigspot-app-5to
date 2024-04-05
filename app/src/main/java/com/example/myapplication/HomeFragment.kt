cb7d392dad7f4d9a4d0efa34e917ed39056206fa
Daniel <126219773+KingOfShit112@users.noreply.github.com>
2024-04-05 14:31:21 -0600

    nothing

--- app/src/main/java/com/example/myapplication/HomeFragment.kt
+++ app/src/main/java/com/example/myapplication/HomeFragment.kt
@@ -1,11 +1,13 @@
 package com.example.myapplication
 
+import android.content.Intent
 import android.os.Bundle
 import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
 
 class HomeFragment : Fragment() {
     val TAG = "HomeFragment"
@@ -18,6 +20,21 @@ class HomeFragment : Fragment() {
         // Inflate the layout for this fragment
         Log.d(TAG,"onCreate: ")
         return inflater.inflate(R.layout.fragment_home, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        Log.d(TAG, "onViewCreated: ")
+
+        // Obtener referencia al ImageView
+        val imageView2: ImageView = view.findViewById(R.id.imageView2)
+
+        // Agregar un click listener al imageView2
+        imageView2.setOnClickListener {
+            // Cuando se hace clic en imageView2, iniciar la EventsActivity
+            val intent = Intent(requireActivity(), EventsActivity::class.java)
+            startActivity(intent)
+        }
     }
 
     override fun onStart() {
---
+++ app/src/main/java/com/example/myapplication/TicketActivity.kt
@@ -0,0 +1,16 @@
+package com.example.myapplication
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+import android.view.View
+import android.widget.ImageView
+
+class TicketActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_ticket)
+    }
+
+}
