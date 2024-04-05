f00768360854bf94174cd053f63e472abb78fcf6
Maria Fernanda Morales <marifer160204@gmail.com>
2024-04-05 00:37:59 -0600

    Finished user frontend

--- app/src/main/java/com/example/myapplication/EventsActivity.kt
+++ app/src/main/java/com/example/myapplication/EventsActivity.kt
@@ -7,5 +7,16 @@ class EventsActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_events)
+
+        // Encontrar la referencia al botón
+        val button = findViewById<button>(R.id.button)
+
+        // Establecer OnClickListener para el botón
+        button.setOnClickListener {
+            // Crear una intención para abrir la actividad TicketActivity
+            val intent = Intent(this, TicketActivity::class.java)
+            // Iniciar la actividad TicketActivity
+            startActivity(intent)
+        }
     }
 }
