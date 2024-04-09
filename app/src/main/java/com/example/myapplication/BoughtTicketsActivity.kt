48f0e2cf1d17de4a1f7899e7f93e999115b7ba91
Maria Fernanda Morales <marifer160204@gmail.com>
2024-04-09 07:48:18 -0600

    Tickets view

---
+++ app/src/main/java/com/example/myapplication/BoughtTicketsActivity.kt
@@ -0,0 +1,11 @@
+package com.example.myapplication
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class BoughtTicketsActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_bought_tickets)
+    }
+}
