448ead46d4de6387efe3d92089c753d623884cc7
Maria Fernanda Morales <marifer160204@gmail.com>
2024-04-08 18:53:47 -0600

    Cardview

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -43,6 +43,7 @@ android {
 dependencies {
 
     //noinspection GradleCompatible
+    implementation("androidx.recyclerview:recyclerview:1.3.2")
     implementation ("com.google.code.gson:gson:2.8.9")
     // implementation ("com.squareup.retrofit2:retrofit:2.9.0")
     // implementation ("com.squareup.retrofit2:converter-gson:2.9.0")
