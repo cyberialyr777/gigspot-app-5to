0f7032a71c20efbf811848c9b1d26b88d90f4d41
Daniel <126219773+KingOfShit112@users.noreply.github.com>
2024-04-10 08:06:13 -0600

    fragmet and recycleView

--- app/src/main/java/com/example/myapplication/HomeBandFragment.kt
+++ app/src/main/java/com/example/myapplication/HomeBandFragment.kt
@@ -1,128 +1,93 @@
 package com.example.myapplication
 
 import android.annotation.SuppressLint
+import android.content.Intent
+import android.database.Cursor
 import android.os.Bundle
 import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.LinearLayout
-import androidx.appcompat.widget.SearchView
-import androidx.recyclerview.widget.GridLayoutManager
+import android.widget.Button
+import android.widget.ImageView
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
-import com.example.myapplication.databinding.FragmentHomeBandBinding
-import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.database.DataSnapshot
 import com.google.firebase.database.DatabaseError
 import com.google.firebase.database.DatabaseReference
 import com.google.firebase.database.FirebaseDatabase
 import com.google.firebase.database.ValueEventListener
-import kotlin.math.log
 
-
-class HomeBandFragment : Fragment(), SearchView.OnQueryTextListener {
+// TODO: Rename parameter arguments, choose names that match
+// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
+private const val ARG_PARAM1 = "param1"
+private const val ARG_PARAM2 = "param2"
+class HomeBandFragment : Fragment() {
     val TAG = "AdapterEventos"
-    private lateinit var auth: FirebaseAuth
-    private lateinit var newArray: ArrayList<Event>
+    private lateinit var newArray: ArrayList<EventModelo>
+    private lateinit var recyclerView: RecyclerView
     private lateinit var dbreferes : DatabaseReference
-    lateinit var myAdapter: CustomAdapterEventsBand
-    private var _binding : FragmentHomeBandBinding? = null
-    private val binding get() = _binding!!
+    private var param1: String? = null
+    private var param2: String? = null
 
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        arguments?.let {
+            param1 = it.getString(ARG_PARAM1)
+            param2 = it.getString(ARG_PARAM2)
+        }
+    }
 
-
+    @SuppressLint("MissingInflatedId")
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        _binding = FragmentHomeBandBinding.inflate(inflater,container,false)
-
-        return binding.root
+        val view = inflater.inflate(R.layout.fragment_home_band, container, false)
+        val reload = view.findViewById<Button>(R.id.showRecycler)
+        recyclerView = view.findViewById(R.id.eventoRecycler)
+        recyclerView.layoutManager = LinearLayoutManager(requireContext())
+        recyclerView.hasFixedSize()
+        newArray = arrayListOf<EventModelo>()
+        reload.setOnClickListener {
+            displayUser()
+        }
+        return view
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        auth = FirebaseAuth.getInstance()
-        newArray = arrayListOf()
-        binding.buscador.setOnQueryTextListener(this)
-        init()
-        getDataEventos("")
+
+    }
+    companion object {
+        // TODO: Rename and change types and number of parameters
+        @JvmStatic
+        fun newInstance(param1: String, param2: String) =
+            HomeBandFragment().apply {
+                arguments = Bundle().apply {
+                    putString(ARG_PARAM1, param1)
+                    putString(ARG_PARAM2, param2)
+                }
+            }
     }
 
-    private fun init() {
-        binding.eventoRecycler.layoutManager = LinearLayoutManager(requireContext(),LinearLayoutManager.VERTICAL,false)
-        myAdapter= CustomAdapterEventsBand(newArray){ evento ->
-            onSelectEvent(evento)
-        }
-        binding.eventoRecycler.adapter = myAdapter
-    }
-
-    private fun filterData(query: String){
-        getDataEventos(query)
-    }
-    fun onSelectEvent(event: Event){
-        val nombreEvento = event.titulo
-        Log.i("selectEvent", "${nombreEvento}")
-    }
-
-    private fun getDataEventos(query: String) {
+    private fun displayUser() {
         dbreferes = FirebaseDatabase.getInstance().getReference("eventos")
-        if(query.isNotEmpty()){
-            dbreferes.orderByChild("emailBand").startAt(query).endAt("${query}/uf8ff").addValueEventListener(object : ValueEventListener{
-                override fun onDataChange(snapshot: DataSnapshot) {
-                    Log.d("firebaseResul","Exitoso snapshot")
-                    newArray.clear()
-                    if(snapshot.exists()){
-                        for (dataSnapshot in snapshot.children){
-                            val data = dataSnapshot.getValue(Event::class.java)
-                            data?.let{
-                                newArray.add(it)
-                            }
-                        }
-                        myAdapter.notifyDataSetChanged()
+        dbreferes.addValueEventListener(object : ValueEventListener {
+            override fun onDataChange(snapshot: DataSnapshot) {
+                if(snapshot.exists()){
+                    for (itemSnapshot in snapshot.children) {
+                        val evento = itemSnapshot.getValue(EventModelo::class.java)
+                        newArray.add(evento!!)
                     }
+                    recyclerView.adapter = CustomAdapterEventsBand(newArray)
                 }
-
-                override fun onCancelled(error: DatabaseError) {
-                    Log.w("firebase", "Failed to read value.", error.toException())                }
-
-            })
-        }else{
-            Log.d("firebaseResul","Exitoso sin query")
-            dbreferes.orderByChild("emailBand").equalTo(auth.currentUser?.email).addValueEventListener(object : ValueEventListener{
-                override fun onDataChange(snapshot: DataSnapshot) {
-                    Log.d("firebaseResul","Exitoso  onDataChange")
-                    newArray.clear()
-                    if(snapshot.exists()){
-                        for(dataSnapshot in snapshot.children){
-                            Log.d("firebaseResul","Exitoso  datasnapshot")
-                            val data = dataSnapshot.getValue(Event::class.java)
-                            data?.let {
-                                newArray.add(it)
-                            }
-                        }
-                        myAdapter.notifyDataSetChanged()
-                    }
-                }
-
-                override fun onCancelled(error: DatabaseError) {
-                    Log.w("firebase", "Failed to read value.", error.toException())                }
-
-            })
-        }
+            }
+            override fun onCancelled(databaseError: DatabaseError) {
+                Log.e("Firebase", "Error al obtener datos: ${databaseError.message}")
+            }
+        })
     }
 
-    override fun onQueryTextSubmit(query: String?): Boolean {
-        if (!query.isNullOrEmpty()){
-            filterData(query)
-        }
-        return true
-    }
-    override fun onQueryTextChange(newText: String?): Boolean {
-        newText?.let { filterData(it) }
-        return true
-        }
 }
-
