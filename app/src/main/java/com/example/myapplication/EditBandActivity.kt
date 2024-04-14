e2e6094db05cd77058a7cc352a1ee7d2e3db566d
Daniel <126219773+KingOfShit112@users.noreply.github.com>
2024-04-14 14:57:19 -0600

    updated image fix

--- app/src/main/java/com/example/myapplication/EditBandActivity.kt
+++ app/src/main/java/com/example/myapplication/EditBandActivity.kt
@@ -4,6 +4,7 @@ import android.app.DatePickerDialog
 import android.app.TimePickerDialog
 import android.content.ContentValues.TAG
 import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
 import android.util.Log
 import android.widget.Button
@@ -11,6 +12,7 @@ import android.widget.DatePicker
 import android.widget.TimePicker
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import com.example.myapplication.Modelos.BandaModelo
 import com.example.myapplication.Modelos.Event
 import com.example.myapplication.databinding.ActivityEditBandBinding
 import com.google.firebase.database.DataSnapshot
@@ -18,6 +20,7 @@ import com.google.firebase.database.DatabaseError
 import com.google.firebase.database.DatabaseReference
 import com.google.firebase.database.FirebaseDatabase
 import com.google.firebase.database.ValueEventListener
+import com.google.firebase.storage.FirebaseStorage
 import com.squareup.picasso.Picasso
 import java.util.*
 
@@ -25,6 +28,9 @@ class EditBandActivity : AppCompatActivity(), DatePickerDialog.OnDateSetListener
     TimePickerDialog.OnTimeSetListener {
     private lateinit var binding: ActivityEditBandBinding
     private lateinit var dbreferes : DatabaseReference
+    val storage = FirebaseStorage.getInstance()
+    private var selectedImageUri: Uri? = Uri.parse("")
+    private var selectedImageUrl: String? = ""
     private val MAP_REQUEST_CODE = 1001
 
 
@@ -54,6 +60,9 @@ class EditBandActivity : AppCompatActivity(), DatePickerDialog.OnDateSetListener
         binding.time.setOnClickListener {
             showTimePickerDialog()
         }
+        binding.guardarImagen.setOnClickListener {
+            pickImageGallery()
+        }
 
         binding.button4.setOnClickListener {
             actualizar(id)
@@ -74,6 +83,21 @@ class EditBandActivity : AppCompatActivity(), DatePickerDialog.OnDateSetListener
         }
 
         cosultaEvento(id, selectedAddress)
+    }
+
+    private fun pickImageGallery() {
+        val intent = Intent(Intent.ACTION_PICK)
+        intent.type = "image/*"
+        startActivityForResult(intent, AddBandFragment.IMAGE_REQUEST_CODE)
+    }
+    @Deprecated("Deprecated in Java")
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if(requestCode == AddBandFragment.IMAGE_REQUEST_CODE && resultCode == RESULT_OK){
+            selectedImageUri = data?.data
+            binding.imagenSeleccionada?.setImageURI(selectedImageUri)
+            Log.d("UriImage", "URI: "+"$selectedImageUri")
+        }
     }
 
     private fun showDatePickerDialog() {
@@ -164,34 +188,68 @@ class EditBandActivity : AppCompatActivity(), DatePickerDialog.OnDateSetListener
         // Encuentra el evento con el ID especificado.
         databaseReference.orderByChild("id").equalTo(id).addListenerForSingleValueEvent(object : ValueEventListener {
             override fun onDataChange(dataSnapshot: DataSnapshot) {
-                // Recorre los eventos encontrados con el ID especificado.
-                for (snapshot in dataSnapshot.children) {
-                    // Obtén el objeto `Event` del `DataSnapshot`.
-                    val evento = snapshot.getValue(Event::class.java)
-                    val eventId = evento?.id
 
-                    // Solo actualiza si el evento con el ID especificado existe.
-                    if (eventId != null && eventId == id) {
-                        // Crea un `HashMap` para almacenar las actualizaciones.
-                        val actualizacionMap = hashMapOf<String, Any?>(
-                            "date" to binding.Fecha.text.toString().trim(),
-                            "time" to binding.time.text.toString().trim(),
-                            "place" to binding.spinner8.text.toString().trim(),
-                            "price" to binding.precio.text.toString().trim(),
-                            "description" to binding.descripcion.text.toString().trim(),
-                            "titulo" to binding.titulo.text.toString().trim()
-                        )
-                        snapshot.ref.updateChildren(actualizacionMap)
-                            .addOnSuccessListener {
-                                val intent = Intent(this@EditBandActivity, EventBandActivity::class.java)
-                                intent.putExtra("ID_EVENTO", id)
-                                finish()
-                                startActivity(intent)
-                                Toast.makeText(contexto, "Actualización exitosa", Toast.LENGTH_SHORT).show()
+
+
+                val storageRef = storage.reference.child("images").child("${UUID.randomUUID()}")
+                storageRef.putFile(selectedImageUri!!).addOnSuccessListener { taskSnapshot ->
+                    storageRef.downloadUrl.addOnSuccessListener { downloadUri ->
+                        val downloadUrl = downloadUri.toString()
+                        selectedImageUrl = downloadUrl
+                        Log.d("UriImage1", "URI de la imagen 1: $selectedImageUri")
+
+                        databaseReference.orderByChild("id").equalTo(id).addListenerForSingleValueEvent(object : ValueEventListener {
+                            override fun onDataChange(dataSnapshot: DataSnapshot) {
+                                for (snapshot in dataSnapshot.children) {
+                                    val user = snapshot.getValue(Event::class.java)
+                                    if (user != null && user.id == id) {
+                                        val actualizacionMap = hashMapOf<String, Any?>(
+                                            "imagen" to selectedImageUrl,
+                                        )
+                                        snapshot.ref.updateChildren(actualizacionMap)
+                                            .addOnSuccessListener {
+
+                                                Log.d("UriImage1", "URI de la imagen 1: $selectedImageUrl")
+                                                for (snapshot2 in dataSnapshot.children) {
+                                                    // Obtén el objeto `Event` del `DataSnapshot`.
+                                                    val evento = snapshot2.getValue(Event::class.java)
+                                                    val eventId = evento?.id
+
+                                                    // Solo actualiza si el evento con el ID especificado existe.
+                                                    if (eventId != null && eventId == id) {
+                                                        // Crea un `HashMap` para almacenar las actualizaciones.
+                                                        val actualizacionMap2 = hashMapOf<String, Any?>(
+                                                            "date" to binding.Fecha.text.toString().trim(),
+                                                            "time" to binding.time.text.toString().trim(),
+                                                            "place" to binding.spinner8.text.toString().trim(),
+                                                            "price" to binding.precio.text.toString().trim(),
+                                                            "description" to binding.descripcion.text.toString().trim(),
+                                                            "titulo" to binding.titulo.text.toString().trim()
+                                                        )
+                                                        snapshot.ref.updateChildren(actualizacionMap2)
+                                                            .addOnSuccessListener {
+                                                                val intent = Intent(this@EditBandActivity, EventBandActivity::class.java)
+                                                                intent.putExtra("ID_EVENTO", id)
+                                                                finish()
+                                                                startActivity(intent)
+                                                                Toast.makeText(contexto, "Successfully update", Toast.LENGTH_SHORT).show()
+                                                            }
+                                                            .addOnFailureListener {
+                                                                Toast.makeText(contexto, "Error", Toast.LENGTH_SHORT).show()
+                                                            }
+                                                    }
+                                                }
+                                            }
+                                            .addOnFailureListener {
+                                                Toast.makeText(this@EditBandActivity, "Error", Toast.LENGTH_SHORT).show()
+                                            }
+                                    }
+                                }
                             }
-                            .addOnFailureListener {
-                                Toast.makeText(contexto, "Error al actualizar", Toast.LENGTH_SHORT).show()
+                            override fun onCancelled(error: DatabaseError) {
+                                TODO("Not yet implemented")
                             }
+                        })
                     }
                 }
             }
